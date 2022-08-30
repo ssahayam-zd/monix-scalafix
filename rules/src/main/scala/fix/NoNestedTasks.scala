c0babe76cc24c646435468f926ec6fca94796ab3
sanjiv.sahayam <sanjiv.sahayam@zendesk.com>
2022-08-30 15:40:14 +1000

    Clean up

--- rules/src/main/scala/fix/NoNestedTasks.scala
+++ rules/src/main/scala/fix/NoNestedTasks.scala
@@ -17,15 +17,12 @@ class NoNestedTasks extends SemanticRule("NoNestedTasks") {
   val TASK_PURE_FUNC = SymbolMatcher.exact("monix/eval/Task.pure().")
 
   override def fix(implicit doc: SemanticDocument): Patch = {
-    println("=================> Tree.structure: " + doc.tree.structureLabeled)
+    // println("=================> Tree.structure: " + doc.tree.structureLabeled)
     doc.tree.collect {
       case  Term.Apply(TASK_M(x), (nested @ TASK_M(y)) :: _) =>
-        println(s"====================== MATCHED!!! $x $y ${nested.structure}, ${nested.pos}") 
         Patch.lint(NestedTaskDiagnostic(nested))
 
       case  Term.Apply(TASK_M(x), (nested @ Term.Apply(arg, _)) :: _) =>
-          println(s"====================== matched2: $x($arg)")             
-          println("Tree.structureLabeledz2: " + arg.structureLabeled)
           val aSymbol: Symbol = arg.symbol
 
           val symbolInfo = doc.info(aSymbol)
@@ -39,35 +36,7 @@ class NoNestedTasks extends SemanticRule("NoNestedTasks") {
                 
             case _ => Patch.empty
           }
-      // case  b @ Term.Block(args) => 
-      //     println(s"====================== blocky1: (${args.structure})")           
-      //     val bSymbol: Symbol = b.symbol
-      //     val symbolInfo = doc.info(bSymbol)
-      //     val syn = b.synthetics
-
-      //     println(s"====================== blocky2: (${symbolInfo}) ${syn}")           
-
-      //     val first = args.head
-      //     val firstSymbol: Symbol = first.symbol
-      //     val firstSymbolInfo = doc.info(firstSymbol)
-      //     val owner = firstSymbolInfo.map(_.owner)
-      //     val syntx = first.syntax
-
-      //     firstSymbolInfo match {
-      //       case Some(si) => 
-      //         si.signature match {
-      //           case MethodSignature(_, params, TypeRef(prefix, sym, ta)) => println(s"====================== blocky3: signature_class=(${si.signature.getClass} prefix=${prefix} symbol:${sym} symbol_display_name:${sym.displayName} ${ta} params=${params} owner=${owner} symbol_info:${sym.info} syntax=$syntx")           
-      //           case other => println(s"====================== blocky5: (${other.getClass})")           
-      //         }
-                   
-      //       case None => println(s"====================== blocky6: (${firstSymbol}), ${firstSymbolInfo.getClass}")           
-      //     }
           
-          
-      //     Patch.empty
-
-
-
       case Term.Apply(
             Term.Select(
               _,
